644e38bd5e2c10b88b3b0d4d84705103970d24e5
iw0017 <uttam.devani@identixweb.in>
2022-05-11 17:19:21 +0530

    commit

--- controllers/insertdataapi.js
+++ controllers/insertdataapi.js
@@ -1,8 +1,19 @@
 const client = require("./connection.js");
+const Joi = require('joi');
 
 const insertData = (req,res) => {
-    
     const {id,first_name,last_name,city} = req.body;
+    
+    const schema = Joi.object({
+        first_name: Joi.string().min(3).max(15).required()
+    });
+
+    let result = schema.validate(req.body);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
     const insQ = `INSERT INTO users (id,first_name,last_name,city) VALUES 
                 (${id},'${first_name}','${last_name}','${city}') `;
     //res.json({query:insQ});
